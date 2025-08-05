f9b18388e2b3ebfc6ca1a9293e631eedfe861dfc
Ruby <rubyabayasonza@gmail.com>
2025-08-05 09:57:58 -0400

    Added build section to vite.config.js for all HTML files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     base: '/auralyst/', 
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                actives: 'actives.html',
+                my_routine: 'my_routine.html',
+                products: 'products.html',
+                routine: 'routine.html',
+            },
+        },
+    },
+
     plugins: [
         tailwindcss()
     ]
